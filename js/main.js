367316162377cd43dbee4171bcad575ba4324047
JamerM <jmartinez6428@misena.edu.co>
2022-11-08 22:01:45 -0500

    aprendiendo alerts en javascript

---
+++ js/main.js
@@ -0,0 +1,14 @@
+alert("Hola Mundo estoy aprendiendo Javascript"); 
+alert("Dos"); 
+alert("Tres"); 
+alert("Hola Mundo estoy aprendiendo Javascript"); 
+
+// Alt + Shift +A   comentar bloque
+console.log("este es un mensaje por consola");
+
+// TIPOS DE DATOS
+console.log(123546)
+console.log("Jamer")
+console.log(true)
+
+document.write("Hola estoy escribiendo en la pagina web");
